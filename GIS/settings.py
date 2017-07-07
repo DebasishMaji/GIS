9a904e39e472b903814688bb2b3cfbf34e4d0ac2
DMaji <debasish@zopper.com>
2017-07-07 21:58:34 +0530

    role created in postgres and corresponding setup configured

--- GIS/settings.py
+++ GIS/settings.py
@@ -76,12 +76,11 @@ WSGI_APPLICATION = 'GIS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+         'ENGINE': 'django.contrib.gis.db.backends.postgis',
+         'NAME': 'gis',
+         'USER': 'gis',
+    },
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
